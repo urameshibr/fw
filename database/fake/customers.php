e66c248e30010a6901d0615e215b94620bfaf027
Lucas Rodrigues <lucas.opensource@gmail.com>
2016-11-23 18:46:20 -0200

    Update customers.php

--- database/fake/customers.php
+++ database/fake/customers.php
@@ -3,20 +3,25 @@
 return [
     'customers' => [
         ['name' => 'Lucas Rodrigues', 'phone_number' => '(17) 9 9224 0632', 'type' => 'PF',
-            'addresses' => ['R: Natal Zambom, 129, Vila Rodrigues, Olímpia, SP', 'R: 19 de Julho, 175, Centro, São José do Rio Preto, SP',],
-            'emails' => ['lucas.opensource@gmail.com', 'laracoders@gmail.com', 'lucas.opensource@hotmail.com']
+            'addresses' => ['R: AAAAAAAAA, 123, AAAAAAAAAAAAA, Olímpia, SP', 'R: BBBBBBBBBBBBB, 4565, Centro, BBBBBBBBBBBBBBB, SP',],
+            'emails' => ['AAAAAAAAAAAAA@gmail.com', 'AAAAAAAAAAA@gmail.com', 'AAAAAAAAAAAAAA@hotmail.com']
         ],
         ['name' => 'Lucas Rodrigues', 'phone_number' => '(17) 9 9224 0632', 'type' => 'PF',
-            'addresses' => ['R: Natal Zambom, 129, Vila Rodrigues, Olímpia, SP', 'R: 19 de Julho, 175, Centro, São José do Rio Preto, SP',],
-            'emails' => ['lucas.opensource@gmail.com', 'laracoders@gmail.com', 'lucas.opensource@hotmail.com']
+            'addresses' => ['R: AAAAAAAAA, 123, AAAAAAAAAAAAA, Olímpia, SP', 'R: BBBBBBBBBBBBB, 4565, Centro, BBBBBBBBBBBBBBB, SP',],
+            'emails' => ['AAAAAAAAAAAAA@gmail.com', 'AAAAAAAAAAA@gmail.com', 'AAAAAAAAAAAAAA@hotmail.com']
         ],
         ['name' => 'Lucas Rodrigues', 'phone_number' => '(17) 9 9224 0632', 'type' => 'PF',
-            'addresses' => ['R: Natal Zambom, 129, Vila Rodrigues, Olímpia, SP', 'R: 19 de Julho, 175, Centro, São José do Rio Preto, SP',],
-            'emails' => ['lucas.opensource@gmail.com', 'laracoders@gmail.com', 'lucas.opensource@hotmail.com']
+            'addresses' => ['R: AAAAAAAAA, 123, AAAAAAAAAAAAA, Olímpia, SP', 'R: BBBBBBBBBBBBB, 4565, Centro, BBBBBBBBBBBBBBB, SP',],
+            'emails' => ['AAAAAAAAAAAAA@gmail.com', 'AAAAAAAAAAA@gmail.com', 'AAAAAAAAAAAAAA@hotmail.com']
         ],
         ['name' => 'Lucas Rodrigues', 'phone_number' => '(17) 9 9224 0632', 'type' => 'PF',
-            'addresses' => ['R: Natal Zambom, 129, Vila Rodrigues, Olímpia, SP', 'R: 19 de Julho, 175, Centro, São José do Rio Preto, SP',],
-            'emails' => ['lucas.opensource@gmail.com', 'laracoders@gmail.com', 'lucas.opensource@hotmail.com']
+            'addresses' => ['R: AAAAAAAAA, 123, AAAAAAAAAAAAA, Olímpia, SP', 'R: BBBBBBBBBBBBB, 4565, Centro, BBBBBBBBBBBBBBB, SP',],
+            'emails' => ['AAAAAAAAAAAAA@gmail.com', 'AAAAAAAAAAA@gmail.com', 'AAAAAAAAAAAAAA@hotmail.com']
         ],
+        ['name' => 'Lucas Rodrigues', 'phone_number' => '(17) 9 9224 0632', 'type' => 'PF',
+            'addresses' => ['R: AAAAAAAAA, 123, AAAAAAAAAAAAA, Olímpia, SP', 'R: BBBBBBBBBBBBB, 4565, Centro, BBBBBBBBBBBBBBB, SP',],
+            'emails' => ['AAAAAAAAAAAAA@gmail.com', 'AAAAAAAAAAA@gmail.com', 'AAAAAAAAAAAAAA@hotmail.com']
+        ],
+        
     ]
 ];
